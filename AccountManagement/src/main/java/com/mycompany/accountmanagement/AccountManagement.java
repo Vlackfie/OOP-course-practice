94ff74070bdd0b2bfc4abc5627015d38306186f9
Sadikul Hossain <sadikulhossain45new@gmail.com>
2026-03-24 20:03:20 +0600

    Update AccountManagement.java

--- AccountManagement/src/main/java/com/mycompany/accountmanagement/AccountManagement.java
+++ AccountManagement/src/main/java/com/mycompany/accountmanagement/AccountManagement.java
@@ -12,7 +12,7 @@ public class AccountManagement {
 
     public static void main(String[] args) {
         BankAccount b1 = new BankAccount("NRB","886","jaf","Sadi","001","Jaf", 5000);
-        BankAccount b2 = new BankAccount("NRB","886","jaf","Miss Maam","001","Jaf", 100000);
+        BankAccount b2 = new BankAccount("NRB","886","jaf","Miss Marvel","001","Jaf", 100000);
         b1.withdraw(5000);
         b1.transfer(b2,900);
     }
